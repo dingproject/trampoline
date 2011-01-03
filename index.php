d6f2577c367a242790c79f021ae2b203ea379955
Hasse R. Hansen <hasse@ramlev.dk>
2011-01-03 12:30:45 +0100

    Changing directory to base directory after loading trampoline hooks

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 /**
  * Trampoline dispatcher
  */
-
 // Load trampoline
 require './trampoline.inc';
 require './trampoline.drupal-default.inc';
@@ -13,6 +12,8 @@ require trampoline_files_path().'/trampoline.config.inc';
 
 // Load Drupal utility functions
 require trampoline_base_path().'/includes/common.inc';
+
+chdir($_SERVER['DOCUMENT_ROOT']);
 
 // Execute trampoline
 trampoline_run($trampoline_config);
