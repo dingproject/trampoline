1d145a320b0e40f4a1cdaf439c5207d1f0e8de7a
Kasper Garnæs <kasper@reload.dk>
2011-01-04 22:50:43 +0100

    Added support for custom cache handlers from $cache_inc variable

--- index.php
+++ index.php
@@ -11,10 +11,19 @@ require trampoline_files_path().'/trampoline.config.inc';
 
 // Load Drupal utility functions
 require trampoline_base_path().'/includes/common.inc';
+
 // Load modified Drupal functions
 require './trampoline.drupal-mod.inc';
 
 chdir($_SERVER['DOCUMENT_ROOT']);
+
+// Load custom cache handler if defined in $cache_inc from settings.php.
+// Otherwise we use a Trampoline implementation using static variables.
+//  
+// Note that custom cache handlers must handle all communication with the 
+// cache engine on it's own. Trampoline does not provide any default Drupal 
+// niceties such as a database connection and abstraction layer.
+require variable_get('cache_inc', dirname(__FILE__).'/trampoline.cache.inc');
 
 // Execute trampoline
 trampoline_run($trampoline_config);
